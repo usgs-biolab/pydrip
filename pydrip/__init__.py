63c1aec5815ffd37050be38c6cb7c105753485b6
dwief <dwieferich@usgs.gov>
2020-06-29 15:02:46 -0600

    add pytest, standardize docstrings drip_sources

--- pydrip/__init__.py
+++ pydrip/__init__.py
@@ -1,18 +1,5 @@
-# pydrip package
+"""Top-level package for publink."""
 
-import pkg_resources
-
-from . import drip_sources
-from . import drip_dam
-from . import bis_pipeline
-
-# from . import drip_pipeline
-
-# __version__ = pkg_resources.require("pydrip")[0].version
-
-
-# def get_package_metadata():
-#    d = pkg_resources.get_distribution('pydrip')
-#    for i in d._get_metadata(d.PKG_INFO):
-#        print(i)
-
+__author__ = """Daniel Wieferich"""
+__email__ = "dwieferich@usgs.gov"
+__version__ = "__version__ = '0.1'"
